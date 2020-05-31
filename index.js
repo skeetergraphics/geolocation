dd9c01962717ea8d36e9cf540f85c40b68416513
skeetergraphics <skeetergraphics@gmail.com>
2020-05-31 09:35:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const Datastore = require('nedb');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Starting server at ' + port);
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+    console.log('I got a request');
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
